59e5eac7ae8392b36700b8bd005ed54e768b1653
wusuowe <wuyou1979@163.com>
2014-04-01 15:07:45 +0800

    create investor app

--- mysite/investor/models.py
+++ mysite/investor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+class Magic(models.Model):
+    code = models.CharField(max_length=6)
+    rdate = models.DateField()
+    name = models.CharField(max_length=16)
+    industry = models.CharField(max_length=64)
+    roic = models.FloatField(default=0.0)
+    rotc = models.FloatField(default=0.0)
+    roicrank =models.IntegerField(default=0)
+    rotcrank =models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
+    class Meta:
+        unique_together = ('code','rdate')
 # Create your models here.
